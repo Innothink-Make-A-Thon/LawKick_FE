d2328c657fa1071b437ecc8824f9be40de80ee72
nahyeon <k92544199@gmail.com>
2023-11-16 04:08:01 +0900

    feat : 네이버 지도 추가, 브랜드 마커와 더미데이터 삽입

---
+++ src/main/main_component/EachMarker.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate} from "react-router-dom";
+
+const EachMarker = () => {
+
+
+    return (
+        <></>
+    );
+};
+
+export default EachMarker;
