dab2afa9f53dc7f02e7d3552b1b95bb67f3f35c5
nahyeon <k92544199@gmail.com>
2023-11-18 12:47:26 +0900

    chore: reportPage 충돌 해결

---
+++ src/select/selectPage.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const SelectPage = () => {
+
+    return (
+        <>
+        </>
+    );
+};
+
+export default SelectPage;
